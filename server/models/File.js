61fae55e4052d145bcb144c8c56bd768526d48f4
DOWRIPILLI PANVISH <119529390+DPanvish@users.noreply.github.com>
2025-12-12 05:24:18 +0530

    File Model

---
+++ server/models/File.js
@@ -0,0 +1,35 @@
+import mongoose from 'mongoose';
+
+const fileSchema = new mongoose.Schema({
+    user: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User',
+        required: true
+    },
+    originalName: {
+        type: String,
+        required: true
+    },
+    mimeType: {
+        type: String,
+        required: true
+    },
+    size: {
+        type: Number,
+        required: true
+    },
+    ipfsHash: {
+        type: String,
+        required: true
+    },
+    encryptionAlgo: {
+        type: String,
+        default: "AES-256-GCM + Kyber1024"
+    },
+    uploadedAt: {
+        type: Date,
+        default: Date.now
+    }
+});
+
+export default mongoose.model("File", fileSchema);
